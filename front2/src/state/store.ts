e5220f335fa35d63064323574060a525b7af0c48
Nivtal <57329093+Nivta@users.noreply.github.com>
2025-01-21 16:44:14 +0200

    Update store.ts

--- front2/src/state/store.ts
+++ front2/src/state/store.ts
@@ -4,10 +4,11 @@ import userReducer from './userSlice';
 
 export const store = configureStore({
   reducer: {
-    user: userReducer,
+    userState: userReducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
 
