c528ce45f5912a271b7e4795c8b54568101c51f1
Nivtal <57329093+Nivta@users.noreply.github.com>
2025-01-21 16:42:09 +0200

    Update PrivateRoute.tsx

--- front2/src/components/PrivateRoute.tsx
+++ front2/src/components/PrivateRoute.tsx
@@ -1,21 +1,17 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate,Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { RootState } from '../state/store'; // יש לוודא ש-RootState מוגדר בצורה הנכונה
 
-interface PrivateRouteProps {
-  children: React.ReactNode; // טיפוס עבור הילדים (מה שיוצג אם יש תוקן)
-}
 
-export default function PrivateRoute({ children }: PrivateRouteProps) {
-  // שליפת התוקן מ-Redux Store
-  const token = useSelector((state: RootState) => state.user.token);
+export default function PrivateRoute() {
+  // שליפת המשתמש מ-Redux Store
+  const user = useSelector((state: RootState) => state.userState.user); // כאן אנחנו בודקים את ה-user
 
-  // אם אין תוקן, הפנה לדף ההתחברות
-  if (!token) {
+  // אם אין משתמש מחובר, הפנה לדף ההתחברות
+  if (!user) {
     return <Navigate to="/" />;
   }
 
-  // אם יש תוקן, הצג את הרכיב המבוקש (children)
-  return <>{children}</>;
+  // אם יש משתמש מחובר, הצג את הרכיב המבוקש (Outlet)
+  return <Outlet />;
 }
